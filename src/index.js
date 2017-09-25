b161f6c1f13e6ab658160dbadcf936de5c057317
Ioannis Geogiadis <Ioannis.Georgiadis@grandparade.co.uk>
2017-09-25 15:52:18 +0200

    before connect tedux react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
-
 
 const tracks = [
     {
@@ -12,8 +13,11 @@ const tracks = [
     }
 ];
 
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
 ReactDOM.render(
-    <Stream tracks={tracks} />,
+    <Stream />,
     document.getElementById('app')
 );
 
